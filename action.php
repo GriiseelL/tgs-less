55d032dceec7c1183c0d2d16ad94353e80743189
Griselda <takun2971@gmail.com>
2024-10-11 12:42:28 +0700

    add index.html

---
+++ action.php
@@ -0,0 +1,7 @@
+<?php
+$nama1 = $_POST["input1"];
+$nama2 = $_POST["input2"];
+
+for($loop = 0; $loop < 3; $loop++) {
+    echo "Halo, " . $nama1, $nama2 . "! <br> ";
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h2>LOGIN</h2> -->
+    <form action="action.php" method="post">
+        <label for="">Angka 1</label> <br>
+            <input type="text" name="input1"> <br>
+        <br>
+
+        <label for="">Angka 2</label> <br>
+            <input type="text" name="input2"> <br>
+        <br> 
+
+        <button>Submit</button>
+    </form>
+</body>
+</html>
