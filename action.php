ef0cd17dc8b0edcd7abdf774a37ba8bec68bed26
Griselda <takun2971@gmail.com>
2024-10-16 18:13:24 +0700

    first

--- action.php
+++ action.php
@@ -1,7 +1,7 @@
 <?php
 $nama1 = $_POST["input1"];
-$nama2 = $_POST["input2"];
+$nama2 = $_POST["input2"];  
 
-for($loop = 0; $loop < 3; $loop++) {
-    echo "Halo, " . $nama1, $nama2 . "! <br> ";
+for($loop = 0; $loop < $nama2; $loop++) {
+    echo "Halo, " . $nama1 . "! <br> ";
 }
